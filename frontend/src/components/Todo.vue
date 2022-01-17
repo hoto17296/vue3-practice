f5250c02429705fe2c4b2cd420b71363d5834f49
hoto <mail@hoto.me>
2022-01-17 10:17:15 +0900

    show list

---
+++ frontend/src/components/Todo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import TodoItem from './TodoItem.vue'
+
+const todo = reactive({
+  list: [{ title: 'foo' }, { title: 'bar' }],
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in todo.list">
+      <TodoItem v-bind="item"></TodoItem>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <span>{{ title }}</span>
+</template>
+
+<style scoped></style>
